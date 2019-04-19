824d024f0cd0b9b284001affa325ec96bec4e879
fcastrogarcia <fede.castrog@gmail.com>
2019-04-19 11:52:19 -0300

    router

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,16 @@ import { Router } from 'express'
 
 const router = Router()
 
+router.route('/')
+    .get(controller)
+    .post(controller)    
+
+
+//
+router.route('/:id')
+    .get(controller) 
+    .put(controller)
+    .delete(controller)    
+
+
 export default router
